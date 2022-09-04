0ac3cdd29f4615740b35ff9eb1219d8aa33f498a
Satyam Joshi <satyam_joshi@epam.com>
2022-09-04 10:58:16 +0530

    inline styling with react

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = ({title, devName}) => {
   return (
     <header>
         <div>
-            <h1>{title} </h1>
+            <h1 style={{color:"red", backgroundColor:"black"}}> {title} </h1>
             <div> ------------------ designed by {devName}</div>
         </div>
     </header>
