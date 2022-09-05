6ba0d7eaf6bf5aee1ba468f841a2b4e9c7746d17
Satyam Joshi <satyam_joshi@epam.com>
2022-09-05 15:28:09 +0530

    Handling Warning: Each child in a list should have a unique "key" prop (for Tasks component.)
    Added 'key' prop for h3

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -22,7 +22,7 @@ const Tasks = () => {
   return (
     
     <>
-      {tasks.map(task => <h3> {task.text} </h3>)}
+      {tasks.map(task => <h3 key={task.id}> {task.text} </h3>)}
     </>
   );
 }
