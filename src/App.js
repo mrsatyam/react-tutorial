61ea95940328d07cf93a5c4da642e468f117b2c1
Satyam Joshi <satyam_joshi@epam.com>
2022-09-05 15:05:45 +0530

    Adding Tasks component to show the tasks using list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
 
   return (
     <div className="container">
       <Header className='header' title='Task Tracker' devName="Satyam" > </Header>
-      <h1> Hello From React</h1>
+      <Tasks> </Tasks>
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const Tasks = () => {
+  const tasks = [
+    {
+      id:1,
+      text: "Doctor's Appointment",
+      day: 'Feb 5th a 2:30 pm',
+      reminder:true
+    },
+    {
+      id:2,
+      text: "Meeting at school",
+      day: 'Feb 6th a 1:30 pm',
+      reminder:true
+    },
+    {
+      id:3,
+      text: "Food shopping",
+      day: 'Feb 5th a 2:30 pm',
+      reminder:false
+    }
+  ];
+  return (
+    
+    <>
+      {tasks.map(task => <h3> {task.text} </h3>)}
+    </>
+  );
+}
+
+export default Tasks;
