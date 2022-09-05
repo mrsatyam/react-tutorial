dd6b73dce86c4d68461acc61c5e216f27e78a40e
Satyam Joshi <satyam_joshi@epam.com>
2022-09-05 16:45:16 +0530

    Introducing "state" in Tasks component with "useState" hook.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,26 +1,27 @@
+import { useState } from "react";
+
 const Tasks = () => {
-  const tasks = [
-    {
-      id:1,
-      text: "Doctor's Appointment",
-      day: 'Feb 5th a 2:30 pm',
-      reminder:true
-    },
-    {
-      id:2,
-      text: "Meeting at school",
-      day: 'Feb 6th a 1:30 pm',
-      reminder:true
-    },
-    {
-      id:3,
-      text: "Food shopping",
-      day: 'Feb 5th a 2:30 pm',
-      reminder:false
-    }
-  ];
-  return (
-    
+const [tasks, setTasks] = useState([
+  {
+    id:1,
+    text: "Doctor's Appointment",
+    day: 'Feb 5th a 2:30 pm',
+    reminder:true
+  },
+  {
+    id:2,
+    text: "Meeting at school",
+    day: 'Feb 6th a 1:30 pm',
+    reminder:true
+  },
+  {
+    id:3,
+    text: "Food shopping",
+    day: 'Feb 5th a 2:30 pm',
+    reminder:false
+  }
+])
+  return (    
     <>
       {tasks.map(task => <h3 key={task.id}> {task.text} </h3>)}
     </>
